4752393c64a1973de440103fa563a1c1ebac9fe5
ravikumar <ravikumar@codegaragetech.com>
2020-08-19 20:00:32 +0530

    fizing issue

--- app/controllers/tracker_management_actions_controller.rb
+++ app/controllers/tracker_management_actions_controller.rb
@@ -126,18 +126,12 @@ class TrackerManagementActionsController < ApplicationController
 						custom_fields_with_ids.each do |key,value|
 
 							tracker_log.info("======> custom field vales:  #{value} <=========")
-
+							
 							tracker_log.info("======> csv field vales:  #{row[key.downcase.strip]} <=========")
 
 							tracker_log.info("======> custom field default_options:  #{value["default_options"] if value['default_options'].present?} <=========")
-							if value["default_options"].present?
-								tracker_log.info("======> inside if condition value:  #{value["default_options"].detect{|a| a.downcase.strip == row[key.downcase.strip] }} <=========")
-								new_input_value = value["default_options"].detect{|a| a.downcase.strip == row[key.downcase.strip].to_s }
-								tracker_log.info("======> inside if custom value:  #{new_input_value} <=========")
-							else
-								new_input_value = row[key.downcase.strip]
-							end
 
+							new_input_value = value["default_options"].present? ?  value["default_options"].detect{|a| a.downcase.strip == row[key.downcase.strip].downcase.strip } : row[key.downcase.strip]
 
 							tracker_log.info("======> custom value:  #{new_input_value} <=========")
 							unless new_input_value.blank?
