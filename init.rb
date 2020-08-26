bfa1c862d52c7a5f51727ba5c4cc10b5aa0d08c4
ravikumar <ravikumar@codegaragetech.com>
2020-08-26 09:48:37 +0530

    changing the icon position

--- init.rb
+++ init.rb
@@ -8,8 +8,9 @@ Redmine::Plugin.register :tracker_management do
 
   	class TrackerManagementHookListener < Redmine::Hook::ViewListener
 	 	# render_on :tracker_management_action_option, :partial => "common_section/tracker_action_option" 
+		render_on :view_issues_index_top,:partial => "common_section/tracker_action_button"
+		# render_on :view_issue_sidebar_top,:partial => "common_section/tracker_action_button"
 	 	render_on :view_issue_sidebar_issue_buttons, :partial => "common_section/tracker_action_option" 
-		render_on :view_issue_sidebar_top,:partial => "common_section/tracker_action_button"
 	end
 
 end
